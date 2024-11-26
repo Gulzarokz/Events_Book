c32d0dd4a394fccfe544922baff0507d95dc10e4
Gulzarokz <gokz22301@gamil.com>
2024-11-26 14:48:28 +0500

    Done events form book with exercise and examples

---
+++ chapter45/index.js
@@ -0,0 +1,27 @@
+// 45 Events: link
+// A good website is a responsive website. The user does something—clicks a button,
+// moves the mouse, presses a key—and something happens. JavaScript gives you many ways to
+// respond to the user's needs. A few examples:
+// The user clicks a button saying "Our Guarantee" and an alert displays spelling out the
+// guarantee
+
+//  What is events
+//All of these user actions—clicking a button, moving the mouse, moving from one field to
+// another in a form, selecting a different option—are known as events.
+
+// What is events handler
+// JavaScript code that responds to an event—for example, displaying a guarantee or swapping an image when the
+// pointer hovers over it—is called an event handler.
+
+// What is inline events
+// Inline event-handling means that you combine bits of JavaScript with HTML markup.
+// Here's a line that displays a link and then displays an alert when the user clicks it.
+
+
+
+
+
+
+
+
+
